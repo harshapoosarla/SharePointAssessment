45fcef83a3cab3b3c2f6c6f8937b8344c33f4e4e
harshapoosarla <43164668+harshapoosarla@users.noreply.github.com>
2018-10-15 14:50:50 +0530

    commit

--- SharePointAssessment/SharePointAssessment/Program.cs
+++ SharePointAssessment/SharePointAssessment/Program.cs
@@ -17,19 +17,21 @@ namespace SharePointAssessment
             using (var ctx = new ClientContext("https://acuvatehyd.sharepoint.com/teams/SharePointDemo1"))
             {
                 ctx.Credentials = new SharePointOnlineCredentials(UserName, Password);
-
                 ExcelPackage(ctx);
             }
         }
         private static void ExcelPackage(ClientContext ctx)
         {
+            //List list = ctx.Web.Lists.GetByTitle("Documents");
+            //Microsoft.SharePoint.Client.File file = list.RootFolder.Files.GetByUrl("SharePointAssessment.xlsx");
+
             Microsoft.SharePoint.Client.File file = ctx.Web.GetFileByUrl("https://acuvatehyd.sharepoint.com/:x:/r/teams/SharePointDemo1/_layouts/15/Doc.aspx?sourcedoc=%7Ba5f7bba6-4627-4bfd-82f5-eccb8e7efd8c%7D&action=default&uid=%7BA5F7BBA6-4627-4BFD-82F5-ECCB8E7EFD8C%7D&ListItemId=4&ListId=%7B3411566D-3EE6-4CB3-9DD4-71B1E7E0AAB3%7D&odsp=1&env=prod");
-            ClientResult<System.IO.Stream> data = file.OpenBinaryStream();
+            ClientResult<Stream> data = file.OpenBinaryStream();
             ctx.Load(file);
             ctx.ExecuteQuery();
             using (var pack = new OfficeOpenXml.ExcelPackage())
             {
-                using (System.IO.MemoryStream memoryStream = new System.IO.MemoryStream())
+                using (MemoryStream memoryStream = new MemoryStream())
                 {
                     if (data != null)
                     {
@@ -56,27 +58,32 @@ namespace SharePointAssessment
                                 int split = filetoupload.LastIndexOf('.');
                                 string lhs = split < 0 ? filetoupload : filetoupload.Substring(0, split);
                                 string rhs = split < 0 ? "" : filetoupload.Substring(split + 1);
-                                System.IO.FileInfo sizeoffile = new System.IO.FileInfo(filetoupload);
+                                //string ups = split < 0 ? "" : filetoupload.Substring(split + 1);
+                                FileInfo sizeoffile = new FileInfo(filetoupload);
                                 long size = sizeoffile.Length;
                                 string filename = sizeoffile.Name;
                                 string deptoffile = WorkSheetRow[rowNumber, 6].Text;
+                                string createdby = WorkSheetRow[rowNumber,3].Text;
+                                string ups = WorkSheetRow[rowNumber, 2].Text;
+                                string[] upssplit = ups.Split(',');
                                 Console.WriteLine(deptoffile);
                                 if ( size <= 1.5e+7)
                                 {
-                                    List documentlibrary = ctx.Web.Lists.GetByTitle("UploadedDocuments");
+                                    List documentlibrary = ctx.Web.Lists.GetByTitle("DemoLib");
+                                    ctx.Load(documentlibrary);
                                     var filecreationinfo = new FileCreationInformation();
                                     filecreationinfo.Content = System.IO.File.ReadAllBytes(filetoupload);
                                     filecreationinfo.Overwrite = true;
-                                    filecreationinfo.Url =filename;
-
+                                    filecreationinfo.Url = Path.Combine("DemoLib/", Path.GetFileName(filename));
                                     Microsoft.SharePoint.Client.File files = documentlibrary.RootFolder.Files.Add(filecreationinfo);
+                                    files.Update();
                                     ctx.ExecuteQuery();
                                     ListItem listItem = files.ListItemAllFields;
                                     listItem["Department"] = deptoffile;
                                     listItem["FileType"] = rhs;
+                                    listItem["CreatedBy"] =createdby;
+                                    listItem["UploadStatus"] =upssplit;
                                     listItem.Update();
-
-                                    ctx.Load(files);
                                     ctx.ExecuteQuery();
                                     Console.WriteLine("Successfully Uploaded");
                                 }
